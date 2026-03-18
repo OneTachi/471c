5f9b5293bdd99c516f3ff1d8cae1b6d05648d143
Sidd <sidd@udel.edu>
2026-03-18 14:34:32 -0400

    complete load & begin

--- packages/L2/src/L2/cps_convert.py
+++ packages/L2/src/L2/cps_convert.py
@@ -5,7 +5,7 @@ from L1 import syntax as L1
 
 from L2 import syntax as L2
 
-# Need to name all intermediate results
+# Need to name all intermediate results and make control flow explicit
 def cps_convert_term(
     term: L2.Term,
     k: Callable[[L1.Identifier], L1.Statement], # A function(name of L1 computation so far) => give us rest of computation
@@ -29,31 +29,61 @@ def cps_convert_term(
             pass
 
         case L2.Immediate(value=value):
-            # Temporary name
+            # Temporary name (not given like in reference!)
             tmp = fresh("t")
             return L1.Immediate(
                 destination=tmp, # Our new temp destination
                 value=value,
                 then=k(tmp) # Rest of computation
             )
-
-        case L2.Primitive(operator=operator, left=left, right=right):
-            pass
+        
+        # Control flow for primitive is to execute the left first and then the right
+        case L2.Primitive(operator=operator, left=left, right=right): # Note we have terms now. So recur them!
+            tmp = fresh("t")
+            # We nest left and right (called left and right for the lambda funcs) because we don't have the final result yet
+            return _term(
+                left,
+                k=lambda left: _term(
+                    right,
+                    k=lambda right: L1.Primitive(
+                        destination=tmp # necc for then
+                        operator=operator,
+                        left=left,
+                        right=right,
+                        then=k(tmp) # Remember that our tmp name was established above, we simply ask for what happens next
+                        # k is our black box (for now) that just tells us what happens next with a variable as input
+                    )
+                )
+            )
 
         case L2.Branch(operator=operator, left=left, right=right, consequent=consequent, otherwise=otherwise):
             pass
-
+        
+        # Similar to Reference
         case L2.Allocate(count=count):
             pass
 
+        # Similar to Primitive
         case L2.Load(base=base, index=index):
-            pass
+            tmp = fresh("t")
+            return _term(
+                base, 
+                k=lambda base: L1.Load( 
+                    # base turns into whatever value the stuff below evaluates to
+                    destination=tmp,
+                    base=base,
+                    index=index,
+                    then=k(tmp)
+                ),
+            )
 
         case L2.Store(base=base, index=index, value=value):
             pass
 
         case L2.Begin(effects=effects, value=value):  # pragma: no branch
-            pass
+            # Make sure that effects stay in order.
+            # L1 Begin doesn't exist. Since our control flow has now been accomplished as value is determined!
+            return _terms(effects, k=lambda effects: _term(value, lambda value: k(value))) # just return our explicit name
 
 
 def cps_convert_terms(
