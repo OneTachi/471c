18f61aad8473d86347da5451c92ced263c20d836
Sidd <sidd@udel.edu>
2026-03-19 14:53:48 -0400

    finish funcs that only check for bindings underneath term

--- packages/L3/src/L3/uniqify.py
+++ packages/L3/src/L3/uniqify.py
@@ -38,7 +38,7 @@ def uniqify_term(
             pass
 
         case Reference(name=name):
-            pass
+            pass            
 
         case Abstract(parameters=parameters, body=body):
             pass
@@ -46,23 +46,35 @@ def uniqify_term(
         case Apply(target=target, arguments=arguments):
             pass
 
-        case Immediate():
-            pass
+        # No bindings, just return as is
+        case Immediate(value=value):
+            return Immediate(value=value)
 
+        # Recur on the unknown terms as they may have bindings.
         case Primitive(operator=operator, left=left, right=right):
-            pass
+            return Primitive(operator=operator, left=_term(left), right=_term(right))
 
+        # These are all terms that we don't know whether has a binding or not. So just recur on them
         case Branch(operator=operator, left=left, right=right, consequent=consequent, otherwise=otherwise):
-            pass
+            return Branch(
+                operator=operator,
+                left=_term(left),
+                right=_term(right),
+                consequent=_term(consequent),
+                otherwise=_term(otherwise)
+            )
+        
+        # count is a natural number, no bindings
+        case Allocate(count=count):
+            return Allocate(count=count)
 
-        case Allocate():
-            pass
-
+        # index is also just a nat, but base is an unknown term. Just recur on it
         case Load(base=base, index=index):
-            pass
-
+            return Load(base=_term(base), index=index)
+        
+        # This does store a value, but doesn't return the binding directly or anything
         case Store(base=base, index=index, value=value):
-            pass
+            return Store(base=_term(base), index=index, value=_term(value))
 
         case Begin(effects=effects, value=value):  # pragma: no branch
             pass
